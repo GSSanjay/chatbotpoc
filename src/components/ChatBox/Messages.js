bf854942d04a29f944caf525dd88adb57ad3db46
Abdul Ziyan <abdulziyan07@gmail.com>
2022-05-09 19:25:35 +0530

    Added : Scroll down fro latest user input

--- src/components/ChatBox/Messages.js
+++ src/components/ChatBox/Messages.js
@@ -1,13 +1,16 @@
 import Message from './Message';
 import PropTypes from 'prop-types';
+import ScrollableFeed from 'react-scrollable-feed';
 
-const Messages = ({ messageData  }) => {
+const Messages = ({ messageData }) => {
   return (
     <div className='box'>
-      {messageData &&
-        messageData.map((message, i) => {
-          return <Message key={i} messageData={message}  />;
-        })}
+      <ScrollableFeed>
+        {messageData &&
+          messageData.map((message, i) => {
+            return <Message key={i} messageData={message} />;
+          })}
+      </ScrollableFeed>
     </div>
   );
 };
